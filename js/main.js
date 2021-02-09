6fa5522a9750df8b456f29eeffd8fe30b873bd0c
KalVasFlame <unnameddwar@gmail.com>
2021-02-09 16:11:22 +0200

    +

---
+++ js/main.js
@@ -0,0 +1,55 @@
+import itemList from './gallery-items.js'
+
+const galleryRef = document.querySelector('.js-gallery');
+const lightboxRef = document.querySelector('.js-lightbox');
+const lightboxImageRef = document.querySelector('.lightbox__content > .lightbox__image');
+const closeModalBtnRef = document.querySelector('button[data-action="close-lightbox"]');
+
+
+
+const createGalleryItem = (items) => {
+  return items.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+    <a
+    class="gallery__link"
+    href='${original}'
+    target='_blank'
+    rel="nofollow noopener"
+  >
+  <img
+  class="gallery__image"
+  src='${preview}'
+  data-source="${original}"
+  alt="${description}"
+  />
+  </a>
+  </li>`}).join(' ')
+};
+const openModal = e => { 
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  lightboxRef.classList.add('is-open');
+  lightboxImageRef.src = `${e.target.dataset.source}`
+
+};
+const closeModal = () => { 
+  lightboxRef.classList.remove('is-open')
+  lightboxImageRef.src = undefined;
+};
+
+
+
+const createMarkup = createGalleryItem(itemList);
+
+
+galleryRef.insertAdjacentHTML('beforeend', createMarkup);
+closeModalBtnRef.addEventListener('click', closeModal)
+galleryRef.addEventListener('click', openModal)
+
+      // Модальное окно для полноразмерного изображения
+      // Для того чтобы открыть, необходимо добавить на div.lightbox CSS-класс is-open
+    //  = itemList.original
+
+    
+// Подмена значения атрибута src элемента img.lightbox__image.
