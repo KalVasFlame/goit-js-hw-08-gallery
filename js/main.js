546ea490c947b0bd5566fb8752bc762e9370901b
KalVasFlame <unnameddwar@gmail.com>
2021-02-15 16:55:59 +0200

    +

--- js/main.js
+++ js/main.js
@@ -7,6 +7,9 @@ const closeModalBtnRef = document.querySelector('button[data-action="close-light
 const lightboxOverlayRef = document.querySelector('.js-lightbox > .lightbox__overlay');
 const bodyRef = document.querySelector('body');
 
+let index = 0;  
+let activeIndex;
+
 const createGalleryItem = items => {
   return items.map(({ preview, original, description }) => {
     return `<li class="gallery__item">
@@ -18,6 +21,7 @@ const createGalleryItem = items => {
   class="gallery__image"
   src='${preview}'
   data-source="${original}"
+  data-index="${index += 1}"
   alt="${description}"
   width = 480
   height = 240
@@ -32,26 +36,59 @@ const onOpenModalBtnCLick = e => {
   }
   e.preventDefault();
   lightboxRef.classList.add('is-open');
-  lightboxImageRef.src = e.target.dataset.source
-
+  lightboxImageRef.src = e.target.dataset.source;
+  lightboxImageRef.alt = e.target.alt;
+  lightboxRef.dataset.index = e.target.dataset.index
+  activeIndex = e.target.dataset.index
 };
 const onCloseModalBtnClick = () => { 
   lightboxRef.classList.remove('is-open')
   lightboxImageRef.src = undefined;
 };
-
-const escCloseModal = e => { 
+const onEscKeydown = e => { 
   if (e.keyCode !== 27) {
   return
   }
   lightboxRef.classList.remove('is-open')
   lightboxImageRef.src = undefined;
 }
+const onLeftArrowBtnKeydown = e => {
+  if (!lightboxRef.classList.contains('is-open')) {
+    return
+  }
+  if (e.keyCode !== 37) { 
+    return
+  }
+  if (activeIndex === 0) { 
+    return
+  }
+  activeIndex -=1
+  lightboxImageRef.src = itemList[activeIndex].original
+  
+}
+const onRightArrowBtnKeydown = e => {
+  if (!lightboxRef.classList.contains('is-open')) {
+    return
+  }
+  if (e.keyCode !== 39) { 
+    return
+  }
+  if (activeIndex === itemList.length - 1) { 
+    return
+  }
+  activeIndex +=1
+  lightboxImageRef.src = itemList[activeIndex].original
+  
+}
 
 const createMarkup = createGalleryItem(itemList);
 
-document.addEventListener('keydown', escCloseModal)
-galleryRef.insertAdjacentHTML('beforeend', createMarkup);
+bodyRef.addEventListener('keydown', onEscKeydown)
+bodyRef.addEventListener('keydown', onLeftArrowBtnKeydown)
+bodyRef.addEventListener('keydown', onRightArrowBtnKeydown)
+
 galleryRef.addEventListener('click', onOpenModalBtnCLick)
 closeModalBtnRef.addEventListener('click', onCloseModalBtnClick)
 lightboxOverlayRef.addEventListener('click', onCloseModalBtnClick)
+
+galleryRef.insertAdjacentHTML('beforeend', createMarkup);
