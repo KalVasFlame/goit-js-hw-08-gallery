108d3c5cc54916fc695d6972f565d16792eb796e
KalVasFlame <unnameddwar@gmail.com>
2021-02-19 17:53:37 +0200

    +

--- dist/hw-8.0a3200e1.js
+++ dist/hw-8.0a3200e1.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59818" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60056" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
